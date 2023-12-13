33106dccbde6b5096b6e5c1d459e72e9fd937c3e
rizkyperman <akrizkypermana@gmail.com>
2023-12-13 15:11:12 +0700

    commit

---
+++ resources/views/Students/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.index')
+@section('content')
+     <div class="container">
+        <div class="row">
+ 
+            <div class="col-md-9">
+                <div class="card">
+                    <div class="card-header">
+                        <h2>Laravel 9 Crud</h2>
+                    </div>
+                    <div class="card-body">
+                        <a href="{{ url('/student/create') }}" class="btn btn-success btn-sm" title="Add New Student">
+                            <i class="fa fa-plus" aria-hidden="true"></i> Add New
+                        </a>
+                        <br/>
+                        <br/>
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Name</th>
+                                        <th>Address</th>
+                                        <th>Mobile</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($students as $item)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $item->name }}</td>
+                                        <td>{{ $item->address }}</td>
+                                        <td>{{ $item->mobile }}</td>
+ 
+                                        <td>
+@endsection
